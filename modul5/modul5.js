2d28b4cd5488330ca75324f9745b07d0bc14db23
ikhsanudinmuh <muhikhsanudin1gmail.com>
2020-01-31 20:48:14 +0700

    modul5

---
+++ modul5/modul5.js
@@ -0,0 +1,47 @@
+var variabel1 = '23'
+var variabel = 23
+
+function namaFungsi() {
+
+}
+
+var tambah = function(a, b) {
+    return a + b
+}
+tambah(2,3)
+
+var kurang = (a,b) => {
+    return a - b
+}
+kurang(3,2)
+
+x = 6
+if(x < 7) {
+
+}
+
+x < 4 ? 'True' : x > 9 ? 'True' : 'False'
+
+switch (x) {
+    case 5 : 'Lima'
+    case 6 : 'Enam'
+    default: 'Else'
+}
+
+for (let i = 0; i < 10; i++){
+
+}
+
+array = [2, 3, 4, 5, 6]
+
+for (item in array) {
+    console.log(item)
+}
+
+while (x < 3) {
+
+}
+
+do {
+
+} while (condition)
